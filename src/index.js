6852238483157d50795ce966ce1fc3845f82acdf
Artyom Chen <artychen13@gmail.com>
2019-06-02 23:43:16 +0900

    setup for React and Webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./javascript/components/App";
+import "./styles/style.scss";
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/(\.css|\.scss|\.sass)$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, 
+                    {
+                        loader: "css-loader"
+                    }, 
+                    {
+                        loader: "sass-loader"
+                    }, 
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|ico)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]'
+                    }
+                  }
+                ]
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 10000,
+                      mimetype: 'application/font-woff'
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 10000,
+                      mimetype: 'application/octet-stream'
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 10000,
+                      mimetype: 'image/svg+xml'
+                    }
+                  }
+                ]
+              },
+        ] 
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: "./src/index.html"
+      })  
+    ]
+};
