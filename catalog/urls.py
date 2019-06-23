0e09bcb58c392d2ba479e6f595463ee679c4b426
PZDuck <agp2281337@gmail.com>
2019-06-23 15:59:01 -0400

    Attempted to integrate React with Django

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 from rest_framework import routers
+import frontend
 
 
 router = routers.DefaultRouter()
@@ -11,5 +12,5 @@ router.register('fields', views.FieldView, 'field')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', views.index),
+    path('', include('frontend.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
